1e00692f754b2db38b4d50070ef01d511e4e5d6e
Caio-Fonseca16 <caiohenriquefonseca0@gmail.com>
2026-09-19 16:40:46 -0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -43,6 +43,15 @@ CREATE TABLE fichatreino(
     observacoes TEXT,
     FOREIGN KEY (idaluno) REFERENCES alunos(idaluno) ON DELETE CASCADE
 );
+CREATE TABLE controle_presenca (
+idpresenca INTEGER PRIMARY KEY AUTOINCREMENT,
+idhorarioaula INTEGER NOT NULL,
+idalunopresente INTEGER NOT NULL,
+presente INTEGER DEFAULT 0 CHECK (presente IN (0, 1)),
+datainscricao TEXT DEFAULT (DATETIME('now', 'localtime')),
+FOREIGN KEY (idhorarioaula) REFERENCES agendaaulas(idaula),
+FOREIGN KEY (idalunopresente) REFERENCES alunos(idaluno),
+);
 
 CREATE TABLE exerciciotreino(
     idexercicio INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -84,3 +93,60 @@ CREATE TABLE avisos(
     lido INTEGER DEFAULT 0 CHECK (lido IN (0,1)),
     FOREIGN KEY (idaluno) REFERENCES alunos(idaluno) ON DELETE CASCADE
 );
+
+DELETE FROM agendaaulas
+WHERE datahorafim < DATETIME('now', 'localtime');
+
+DELETE FROM financeiro
+WHERE statuspagamento = 'Atrasado'
+AND datavencimento < DATE('now', '-90 days');
+
+PRAGMA foreign_keys = ON;
+
+DELETE FROM alunos 
+WHERE idaluno = 1;
+
+CREATE TRIGGER trgvalidarvagas
+BEFORE INSERT ON controlepresenca
+FOR EACH ROW
+BEGIN
+    SELECT CASE
+        WHEN (SELECT vagasdisponiveis FROM agendaaulas WHERE idaula = NEW.idaula) <= 0
+        THEN RAISE(ABORT, 'A aula selecionada não possui mais vagas disponíveis.')
+    END;
+END;
+
+CREATE TRIGGER trgreservavaga
+AFTER INSERT ON controlepresenca
+FOR EACH ROW
+BEGIN
+    UPDATE agendaaulas
+    SET vagasdisponiveis = vagasdisponiveis - 1
+    WHERE idaula = NEW.idaula;
+END;
+
+CREATE TRIGGER trgavisofinanceiroinsert
+AFTER INSERT ON financeiro
+FOR EACH ROW
+WHEN NEW.statuspagamento = 'Pendente' AND NEW.datavencimento <= DATE('now', '+3 days')
+BEGIN
+    INSERT INTO avisos (idaluno, tipoaviso, mensagem)
+    VALUES (
+        NEW.id_aluno,
+        'Mensalidade próxima do vencimento',
+        'Sua fatura no valor de R$ ' || NEW.valor || ' vence em ' || STRFTIME('%d/%m/%Y', NEW.datavencimento)
+    );
+END;
+
+CREATE TRIGGER trgavisofinanceiroupdate
+AFTER UPDATE ON financeiro
+FOR EACH ROW
+WHEN NEW.statuspagamento = 'Pendente' AND NEW.datavencimento <= DATE('now', '+3 days')
+BEGIN
+    INSERT INTO avisos (idaluno, tipoaviso, mensagem)
+    VALUES (
+        NEW.id_aluno,
+        'Mensalidade próxima do vencimento',
+        'Sua fatura no valor de R$ ' || NEW.valor || ' vence em ' || STRFTIME('%d/%m/%Y', NEW.datavencimento)
+    );
+END;
