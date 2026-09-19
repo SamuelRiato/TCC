e4cf795d50eed0934829195406f81ef064c276fa
Caio-Fonseca16 <caiohenriquefonseca0@gmail.com>
2026-09-19 00:07:47 -0300

    "Duas tabelas banco (incompletas)"

--- database.sql
+++ database.sql
@@ -1,10 +1,23 @@
 CREATE DATABASE TCC_StudioFuncional_ThiagoRiato;
 
-CREATE table alunos(
-    idaluno int auto_increment not null primary key,
-    nomealuno varchar (100) not null,
-    telefone varchar(20),
-    idade int check (idade > 0),
-    fotoaluno varchar (255),
-    statusdematricula VARCHAR(20) DEFAULT 'Ativo' CHECK (statusdematricula IN ('Ativo', 'Inativo', 'Trancado', 'Cancelado')),
-     datadocadastro date)
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE alunos (
+    idaluno INTEGER PRIMARY KEY AUTOINCREMENT,
+    nomealuno TEXT NOT NULL,
+    telefonealuno TEXT,
+    idadealuno INTEGER CHECK (idadealuno > 0),
+    fotoaluno TEXT,
+    statusdematricula TEXT DEFAULT 'Ativo' CHECK (statusdematricula IN ('Ativo', 'Inativo', 'Trancado', 'Cancelado'))
+);
+
+CREATE TABLE avaliacaofisica (
+    idavaliacao INTEGER PRIMARY KEY AUTOINCREMENT,
+    idalunoavaliado INTEGER NOT NULL,
+    peso REAL,
+    altura REAL,
+    dataavaliacao TEXT DEFAULT (DATE('now')),
+    peso REAL NOT
+    NULL CHECK (altura > 0 and altura <3.0)
+    FOREIGN KEY (idalunoavaliado) REFERENCES alunos(idaluno) ON DELETE CASCADE
+);
